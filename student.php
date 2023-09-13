5a7bc124991399a18fe6631a7834ec90e0ab4c7f
Deshani Pubudu <144082930+DeshaniPubudu@users.noreply.github.com>
2023-09-12 23:17:04 -0700

    Update student.php

--- student.php
+++ student.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "minthrm";
+$dbname = "student";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
